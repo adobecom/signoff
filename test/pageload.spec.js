b88b631cd49f9523d18092312d00cc4da5045d7c
James Tsay <tsay@adobe.com>
2024-05-15 16:17:21 -0700

    Support Gist input

--- test/pageload.spec.js
+++ test/pageload.spec.js
@@ -1,6 +1,7 @@
-const fs   = require('fs');
-const yaml = require('js-yaml');
-import { test, expect } from '@playwright/test';
+const fs = require("fs");
+const yaml = require("js-yaml");
+const fetch = require("sync-fetch");
+import { test, expect } from "@playwright/test";
 
 test.use({
   contextOptions: {
@@ -8,93 +9,112 @@ test.use({
   },
 });
 
-test.describe.configure({ mode: 'parallel' });
+test.describe.configure({ mode: "parallel" });
 
-let links = yaml.load(fs.readFileSync('urls.yml', 'utf8'));
+let links = null;
 
-if (process.env.ENVIRONMENT === 'stage') {
-  links = links.map(link => link.replace('https://www.', 'https://www.stage.'));
-  links = links.map(link => link.replace('https://business.', 'https://business.stage.'));
+if (process.env.GIST_URLS) {
+  const gistId = process.env.GIST_URLS;
+  const response = fetch(`https://api.github.com/gists/${gistId}`).json();
+  links = Object.values(response.files)[0]
+    .content.split("\n")
+    .map((x) => x.trim());
+} else {
+  links = yaml.load(fs.readFileSync("urls.yml", "utf8"));
 }
 
-const testPageLoad = async ({page}, testInfo) => { 
-    test.setTimeout(600000);
-    const screenshotPath = 'screenshots/';
+if (process.env.ENVIRONMENT === "stage") {
+  links = links.map((link) =>
+    link.replace("https://www.", "https://www.stage.")
+  );
+  links = links.map((link) =>
+    link.replace("https://business.", "https://business.stage.")
+  );
+}
 
-    const url = testInfo.title;
+const testPageLoad = async ({ page }, testInfo) => {
+  test.setTimeout(600000);
+  const screenshotPath = "screenshots/";
 
-    const response = await page.goto(url, { waitUntil: 'networkidle' });
+  const url = testInfo.title;
 
-    await expect(response.status()).toBe(200);
-  
-    const loadOK = page.locator('div#page-load-ok-milo');
-    await expect(loadOK).toHaveCount(1);
+  const response = await page.goto(url, { waitUntil: "networkidle" });
 
-    const screenshotName = url.replace(/^https:\/\//, '').replace(/[^a-z0-9]/gi, '_').toLowerCase();
-    await page.screenshot({ path: `${screenshotPath}/${screenshotName}.png` });
+  await expect(response.status()).toBe(200);
 
-    const allHrefs = await page.evaluate(() => {
-      return Array.from(document.links).map((item) => item.href);
-    });
-  
-    const hrefs = [...new Set(allHrefs)].sort();
-  
-    let output = [];
-    for (let i = 0; i < hrefs.length; i++) {
-      try {
-        // special rules
-        const url = new URL(hrefs[i]);
-        if (hrefs[i].startsWith('tel:') || url.hash === '#open-jarvis-chat') {
-          continue;
-        }
-        
-        let response = await page.goto(hrefs[i]);
-        if (response === null) {
-          response = await page.waitForResponse(() => true, { timeout: 10000 });
-        }
-  
-        for (
-          let request = response.request();
-          request;
-          request = request.redirectedFrom()
-        ) {
-          const message = `${(
-            await request.response()
-          ).status()} ${request.url()}`;
-          output.push(message);
-        }
-      } catch (e) {
-        console.log(e.message);
-        const message = `999 ${hrefs[i]} no errorcode, offline?`;
+  const loadOK = page.locator("div#page-load-ok-milo");
+  await expect(loadOK).toHaveCount(1);
+
+  const screenshotName = url
+    .replace(/^https:\/\//, "")
+    .replace(/[^a-z0-9]/gi, "_")
+    .toLowerCase();
+  await page.screenshot({ path: `${screenshotPath}/${screenshotName}.png` });
+
+  const allHrefs = await page.evaluate(() => {
+    return Array.from(document.links).map((item) => item.href);
+  });
+
+  const hrefs = [...new Set(allHrefs)].sort();
+
+  let output = [];
+  for (let i = 0; i < hrefs.length; i++) {
+    try {
+      // special rules
+      const url = new URL(hrefs[i]);
+      if (hrefs[i].startsWith("tel:") || url.hash === "#open-jarvis-chat") {
+        continue;
+      }
+
+      let response = await page.goto(hrefs[i]);
+      if (response === null) {
+        response = await page.waitForResponse(() => true, { timeout: 10000 });
+      }
+
+      for (
+        let request = response.request();
+        request;
+        request = request.redirectedFrom()
+      ) {
+        const message = `${(
+          await request.response()
+        ).status()} ${request.url()}`;
         output.push(message);
       }
-      if (process.env.REQUEST_DELAY) {
-        const delay = parseInt(process.env.REQUEST_DELAY);
-        await page.waitForTimeout(delay);
-      }
+    } catch (e) {
+      console.log(e.message);
+      const message = `999 ${hrefs[i]} no errorcode, offline?`;
+      output.push(message);
     }
-
-    await page.close();
-  
-    const errors999 = output.filter((item) => item.startsWith('999'));
-    if (errors999.length > 0) {
-      console.log('\n999 errors:');
-      for (let i = 0; i < errors999.length; i++) {
-        console.log(errors999[i]);
-      }
+    if (process.env.REQUEST_DELAY) {
+      const delay = parseInt(process.env.REQUEST_DELAY);
+      await page.waitForTimeout(delay);
     }
-
-    let errors404 = output.filter((item) => item.startsWith('404'));
-
-    if (errors404.length > 0) {
-      let knownIssues = yaml.load(fs.readFileSync('urls_known_issues.yml', 'utf8'));
-      if (knownIssues[url]) {
-        errors404 = errors404.filter(x => !knownIssues[url].includes(x))
-      }
-    }
-
-    await expect(errors404, errors404.join('\n')).toHaveLength(0);
   }
+
+  await page.close();
+
+  const errors999 = output.filter((item) => item.startsWith("999"));
+  if (errors999.length > 0) {
+    console.log("\n999 errors:");
+    for (let i = 0; i < errors999.length; i++) {
+      console.log(errors999[i]);
+    }
+  }
+
+  let errors404 = output.filter((item) => item.startsWith("404"));
+
+  if (errors404.length > 0) {
+    let knownIssues = yaml.load(
+      fs.readFileSync("urls_known_issues.yml", "utf8")
+    );
+    if (knownIssues[url]) {
+      errors404 = errors404.filter((x) => !knownIssues[url].includes(x));
+    }
+  }
+
+  await expect(errors404, errors404.join("\n")).toHaveLength(0);
+};
 
 for (let link of links) {
   test(link, testPageLoad);
