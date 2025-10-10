7e6709d50c1c9452a20b032c5a095d7fc5b9a413
James Tsay <tsay@adobe.com>
2025-10-09 19:09:19 -0700

    Try out a WDIO test

--- wdio.conf.js
+++ wdio.conf.js
@@ -93,11 +93,11 @@ exports.config = {
     // baseUrl: 'http://localhost:8080',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout: 20000,
     //
     // Default timeout in milliseconds for request
     // if browser driver or grid doesn't send response
-    connectionRetryTimeout: 120000,
+    connectionRetryTimeout: 600000,
     //
     // Default request retries count
     connectionRetryCount: 3,
