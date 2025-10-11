3b8108917d21f6af9e89a07c3968ddaf7397e894
James Tsay <tsay@adobe.com>
2025-10-10 22:49:33 -0700

    Update mobile tests

--- wdio-android.conf.js
+++ wdio-android.conf.js
@@ -22,7 +22,7 @@ exports.config = {
     // of the config file unless it's absolute.
     //
     specs: [
-        './**/*.e2e.js'
+        './**/*.android.js'
     ],
     // Patterns to exclude.
     exclude: [
@@ -51,12 +51,9 @@ exports.config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        // capabilities for local Appium web tests on iOS
-        platformName: 'iOS',
-        browserName: 'Safari',
-        'appium:deviceName': 'iPhone 16',
-        'appium:platformVersion': '18.4',
-        'appium:automationName': 'XCUITest'
+        platformName: 'Android',
+        browserName: 'chrome',
+        'appium:automationName': 'UiAutomator2',  
     }],
 
     //
@@ -106,7 +103,14 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['appium'],
+    services: [['appium', {
+        command: 'appium',
+        args: {
+            relaxedSecurity: true,
+            allowInsecure: ['chromedriver_autodownload'],
+            log: './log/appium.log',
+        },        
+    }]],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
