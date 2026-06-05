860a9be052a5668db66ecb6e4494541a15709fb8
James Tsay <tsay@adobe.com>
2026-06-04 22:48:00 -0700

    Fix for LV

--- plans/monitor_row_milo.spec.js
+++ plans/monitor_row_milo.spec.js
@@ -697,7 +697,7 @@ test.describe('Creative Cloud Plans Page Monitoring', () => {
                 normalizedCardPrice = normalizedCardPrice.replace(/[^\d.]/g, '');
               }
               // 33,49 €kuus vs 33,49 € kuus
-              if (['lt', 'ee'].includes(countryCode)) {
+              if (['lt', 'ee', 'lv'].includes(countryCode)) {
                 normalizedCardPrice = normalizedCardPrice.split('€')[0];
               }
               normalizedCardPrice = normalizedCardPrice.replace('Alternatively at ', '');
